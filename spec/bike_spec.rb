f0158c3960fa58bad2d9bf07fc90850d69442615
Nicki <nkeszler10@gmail.com>
2013-11-14 13:52:20 +0000

    Docking station added (fix)

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,23 +1,33 @@
-require 'bike'
+require 'bike' 
+require 'docking_station'
 
 describe Bike do
 
+	let(:bike) {Bike.new}
+
 	it "should not be broken after we create it" do
-		bike = Bike.new
 		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
 
 	it "should be able to be fixed" do
-		bike = Bike.new
 		bike.break
 		bike.fix
 		expect(bike).not_to be_broken
 	end
 
+end
+
+describe DockingStation do
+	it "should accept a bike" do
+		bike = Bike.new
+		station = DockingStation.new
+		expect(station.bike_count).to eq(0)
+		station.dock(bike)
+		expect(station.bike_count).to eq(1)
+	end
 end
