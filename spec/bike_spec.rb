737d3e8101e4724749c410f4205becfa4ae39a34
Nicki <nkeszler10@gmail.com>
2013-11-14 12:39:16 +0000

    Broken? and can break? tests pass

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,15 @@
 require 'bike'
 
-describe Bike do 
+describe Bike do
+
 	it "should not be broken after we create it" do
 		bike = Bike.new
 		expect(bike).not_to be_broken
+	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
 	end
 end
