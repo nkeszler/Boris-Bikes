260047b2ff36e53097ab770a7e7165d8b2ed982c
Nicki <nkeszler10@gmail.com>
2013-11-14 14:15:38 +0000

    Docks can release bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
 class DockingStation
+	DEFAULT_CAPACITY = 10
 
-	def initialize
+	def initialize(options = {})
+		@capacity = options[:capacity] || DEFAULT_CAPACITY
 		@bikes = []
 	end
 
@@ -10,5 +12,13 @@ class DockingStation
 
 	def dock(bike)
 		@bikes << bike
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,4 @@
 require 'bike' 
-require 'docking_station'
 
 describe Bike do
 
@@ -18,16 +17,5 @@ describe Bike do
 		bike.break
 		bike.fix
 		expect(bike).not_to be_broken
-	end
-
-end
-
-describe DockingStation do
-	it "should accept a bike" do
-		bike = Bike.new
-		station = DockingStation.new
-		expect(station.bike_count).to eq(0)
-		station.dock(bike)
-		expect(station.bike_count).to eq(1)
 	end
 end
